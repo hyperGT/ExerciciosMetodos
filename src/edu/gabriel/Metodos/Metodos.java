5ea6bb2b202723aece356576cedac74d3e8cad07
hyperGT <charlesgabriel102@gmail.com>
2023-03-26 14:20:54 -0300

    Todos os métodos estavam reunidos unicamente nesse método. Decidi deixar mais organizado.

--- src/edu/gabriel/Metodos/Metodos.java
+++ src/edu/gabriel/Metodos/Metodos.java
@@ -1,2 +1,22 @@
-package edu.gabriel.Metodos;public class Metodos {
+package edu.gabriel.Metodos;
+
+
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class Metodos {
+
+    /*
+    * Calcule 4 operações basicas, pedindo 2 numeros para realiza-las
+    *
+    * A partir da hora do dia, informe uma mensagem: bom dia, boa tarde, boa noite
+    *
+    * Calcule o valor de um empréstimo solicitado. Taxas e parcelas influenciam.
+    * Defina arbitrariamente as taxas que influenciam nos valores
+    *
+    * Calcule a area dos 3 quadrilateros notáveis: trapezio, quadrado e retangulo, crie um metodo que utilize o conceito de sobre carga
+    * Nota: para criar uma sobrecarga de metodo e so mudar sua lista de parametros
+    * */
+
+
 }
