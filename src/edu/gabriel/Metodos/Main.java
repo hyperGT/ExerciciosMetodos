8a56618d1e48ae66e34574192468122b70d4f701
hyperGT <charlesgabriel102@gmail.com>
2023-03-26 14:18:40 -0300

    Metodo Main padrão

--- src/edu/gabriel/Metodos/Main.java
+++ src/edu/gabriel/Metodos/Main.java
@@ -1,2 +1,12 @@
-package edu.gabriel.Metodos;public class Main {
+package edu.gabriel.Metodos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MensagemDeBomDia.mostraMensagem();
+        CalcularQuadrilatero.operacoesMat();
+        Calculadora.escolhaOperacao();
+
+    }
 }
